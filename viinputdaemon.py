09ddb945b53552762881c34e50df578b18a4dbcb
Kieran Gee <kieran.lost.the.game@gmail.com>
2015-10-27 17:37:07 +0800

    Initial code commit

---
+++ viinputdaemon.py
@@ -0,0 +1,324 @@
+# The MIT License (MIT)
+
+# Copyright (c) 2015 Kieran Gee
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import uinput 	#interface between python and the uinput kernel module
+import time 	#for time.sleep()
+import serial 	#the keyboard this program interfaces with uses serial
+import os
+import sys
+
+# Easier debugging :^)
+class termcolour:
+    PINK = '\033[95m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    WHITE = '\033[0m'
+
+# Figure out what to do on the keypresses
+def F1(inProgram):
+	print termcolour.PINK + 'F1 Pressed' + termcolour.WHITE
+	print termcolour.GREEN + 'Program:' + termcolour.WHITE, inProgram
+	if inProgram == 'nano':  # Open Help
+		device.emit_combo([
+			uinput.KEY_LEFTCTRL,
+			uinput.KEY_G,
+			])
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Help'
+	if inProgram == 'newsbeuter':
+		device.emit_combo([
+			uinput.KEY_LEFTSHIFT,
+			uinput.KEY_SLASH,
+			])
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Help'
+	if inProgram == 'alpine':
+		device.emit_combo([
+			uinput.KEY_LEFTSHIFT,
+			uinput.KEY_SLASH,
+			])
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Help'
+	if inProgram == 'links':
+		device.emit_click(uinput.KEY_F9)
+		time.sleep(0.1)
+		device.emit_click(uinput.KEY_H)
+		time.sleep(0.1)
+		device.emit_click(uinput.KEY_M)
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Help'
+	if inProgram == 'zsh':
+		device.emit_combo([
+			uinput.KEY_LEFTCTRL,
+			uinput.KEY_C,
+			])
+		device.emit_click(uinput.KEY_C)
+		device.emit_click(uinput.KEY_D)
+		device.emit_click(uinput.KEY_ENTER)
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Home'
+
+def F2(inProgram):
+	print termcolour.PINK + 'F2 Pressed' + termcolour.WHITE
+	print termcolour.GREEN + 'Program:' + termcolour.WHITE, inProgram
+	if inProgram == 'nano':  # Open File
+		device.emit_combo([
+			uinput.KEY_LEFTCTRL,
+			uinput.KEY_R,
+			])
+		time.sleep(0.1)
+		device.emit_combo([
+			uinput.KEY_LEFTCTRL,
+			uinput.KEY_T,
+			])
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Open File'
+	if inProgram == 'newsbeuter':
+		device.emit_click(uinput.KEY_ENTER)
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Open'
+	if inProgram == 'alpine':
+		device.emit_click(uinput.KEY_I)
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Index'
+	if inProgram == 'links':
+		device.emit_click(uinput.KEY_G)
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Go To'
+	if inProgram == 'zsh':
+		device.emit_click(uinput.KEY_P)
+		device.emit_click(uinput.KEY_L)
+		device.emit_click(uinput.KEY_A)
+		device.emit_click(uinput.KEY_Y)
+		device.emit_click(uinput.KEY_SPACE)
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'play '
+
+def F3(inProgram):
+	print termcolour.PINK + 'F3 Pressed' + termcolour.WHITE
+	print termcolour.GREEN + 'Program:' + termcolour.WHITE, inProgram
+	if inProgram == 'nano': # Save file
+		device.emit_combo([
+			uinput.KEY_LEFTCTRL,
+			uinput.KEY_O,
+			])
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Save File'
+	if inProgram == 'newsbeuter':
+		device.emit_click(uinput.KEY_S)
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Save Story'
+	if inProgram == 'alpine':
+		device.emit_click(uinput.KEY_C)
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Compose'
+	if inProgram == 'links':
+		device.emit_click(uinput.KEY_F9)
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Menu'
+	if inProgram == 'zsh':
+		device.emit_combo([
+			uinput.KEY_LEFTCTRL,
+			uinput.KEY_C,
+			])
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Cancel'
+
+def F4(inProgram):
+	print termcolour.PINK + 'F4 Pressed' + termcolour.WHITE
+	print termcolour.GREEN + 'Program:' + termcolour.WHITE, inProgram
+	if inProgram == 'nano': # Cancel
+		device.emit_combo([
+			uinput.KEY_LEFTCTRL,
+			uinput.KEY_C,
+			])
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Cancel'
+	if inProgram == 'alpine':
+		device.emit_click(uinput.KEY_M)
+		time.sleep(0.1)
+		device.emit_click(uinput.KEY_COMMA)
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Back'
+	if inProgram == 'links':
+		device.emit_click(uinput.KEY_ESC)
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Cancel'
+
+def F5(inProgram):
+	print termcolour.PINK + 'F5 Pressed' + termcolour.WHITE
+	print termcolour.GREEN + 'Program:' + termcolour.WHITE, inProgram
+	if inProgram == 'nano': # Cut
+		device.emit_combo([
+			uinput.KEY_LEFTCTRL,
+			uinput.KEY_K,
+			])
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Cut'
+	if inProgram == 'newsbeuter':
+		device.emit_click(uinput.KEY_R)
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Reload'
+	if inProgram == 'alpine':
+		device.emit_click(uinput.KEY_J)
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Journal'
+
+def F6(inProgram):
+	print termcolour.PINK + 'F6 Pressed' + termcolour.WHITE
+	print termcolour.GREEN + 'Program:' + termcolour.WHITE, inProgram
+	if inProgram == 'nano': # Uncut
+		device.emit_combo([
+			uinput.KEY_LEFTCTRL,
+			uinput.KEY_U,
+			])
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Uncut'
+	if inProgram == 'newsbeuter':
+		uinput.KEY_N,
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Next Unread'
+	if inProgram == 'alpine':
+		device.emit_click(uinput.KEY_A)
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Address'
+
+def F7(inProgram):
+	print termcolour.PINK + 'F7 Pressed' + termcolour.WHITE
+	print termcolour.GREEN + 'Program:' + termcolour.WHITE, inProgram
+	if inProgram == 'nano': # Find
+		device.emit_combo([
+			uinput.KEY_LEFTCTRL,
+			uinput.KEY_W,
+			])
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Find'
+	if inProgram == 'newsbeuter':
+		device.emit_click(uinput.KEY_O)
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Open in Browser'
+	if inProgram == 'alpine':
+		device.emit_click(uinput.KEY_S)
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Setup'
+	if inProgram == 'links':
+		device.emit_click(uinput.KEY_SLASH)
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Find'
+
+def F8(inProgram):
+	print termcolour.PINK + 'F8 Pressed' + termcolour.WHITE
+	print termcolour.GREEN + 'Program:' + termcolour.WHITE, inProgram
+	if inProgram == 'nano': # Exit menu or program
+		device.emit_combo([
+			uinput.KEY_LEFTCTRL,
+			uinput.KEY_X,
+			])
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Quit'
+	if inProgram == 'newsbeuter':
+		device.emit_click(uinput.KEY_Q)
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Quit'
+	if inProgram == 'alpine':
+		device.emit_click(uinput.KEY_Q)
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Quit'
+	if inProgram == 'links':
+		device.emit_click(uinput.KEY_Q)
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Quit'
+	if inProgram == 'zsh':
+		device.emit_click(uinput.KEY_LEFTCTRL)
+		device.emit_click(uinput.KEY_D)
+		print termcolour.GREEN + 'Command:' + termcolour.WHITE, 'Quit'
+
+# Main Program, is there a main method in python?
+print termcolour.PINK + '~ Daemon initialising ~' + termcolour.WHITE
+
+# Check if program was run with an arguement
+if len(sys.argv) > 1:
+	print termcolour.GREEN + 'Argument:' + termcolour.WHITE, str(sys.argv[1])
+	program = str(sys.argv[1])
+else:
+	print termcolour.YELLOW + 'No args, what are you even doing?' + termcolour.WHITE
+	program = ''
+
+# Python-uinput is a quality Interface
+device = uinput.Device([
+	uinput.KEY_A,
+	uinput.KEY_B,
+	uinput.KEY_C,
+	uinput.KEY_D,
+	uinput.KEY_E,
+	uinput.KEY_F,
+	uinput.KEY_G,
+	uinput.KEY_H,
+	uinput.KEY_I,
+	uinput.KEY_J,
+	uinput.KEY_K,
+	uinput.KEY_L,
+	uinput.KEY_M,
+	uinput.KEY_N,
+	uinput.KEY_O,
+	uinput.KEY_P,
+	uinput.KEY_Q,
+	uinput.KEY_R,
+	uinput.KEY_S,
+	uinput.KEY_T,
+	uinput.KEY_U,
+	uinput.KEY_V,
+	uinput.KEY_W,
+	uinput.KEY_X,
+	uinput.KEY_Y,
+	uinput.KEY_Z,
+	uinput.KEY_1,
+	uinput.KEY_2,
+	uinput.KEY_3,
+	uinput.KEY_4,
+	uinput.KEY_5,
+	uinput.KEY_6,
+	uinput.KEY_7,
+	uinput.KEY_8,
+	uinput.KEY_9,
+	uinput.KEY_0,
+	uinput.KEY_TAB,
+	uinput.KEY_ENTER,
+	uinput.KEY_SPACE,
+	uinput.KEY_DOT,
+	uinput.KEY_COMMA,
+	uinput.KEY_SLASH,
+	uinput.KEY_BACKSLASH,
+	uinput.KEY_LEFTCTRL,
+	uinput.KEY_LEFTALT,
+	uinput.KEY_LEFTSHIFT,
+	uinput.KEY_F1,
+	uinput.KEY_F2,
+	uinput.KEY_F3,
+	uinput.KEY_F4,
+	uinput.KEY_F5,
+	uinput.KEY_F6,
+	uinput.KEY_F7,
+	uinput.KEY_F8,
+	uinput.KEY_F9,
+	uinput.KEY_F10,
+	uinput.KEY_F11,
+	uinput.KEY_F12,
+    ])
+
+# Open serial decice
+ser = serial.Serial('/dev/ttyUSB0', 115200, timeout = 1)
+print termcolour.GREEN + 'Serial device opened:' + termcolour.WHITE, ser.name
+
+# Mad Hacks go here
+
+# Polling for input
+while 1:
+	sbuf = ser.read()
+	print 'Buffer Queue =', ser.inWaiting()
+	print 'Read =', sbuf
+	
+	if sbuf == '\x81': #129
+		F1(program)
+	if sbuf == '\x82': #130
+		F2(program)
+	if sbuf == '\x83': #131
+		F3(program)
+	if sbuf == '\x84': #132
+		F4(program)
+	if sbuf == '\x85': #133
+		F5(program)
+	if sbuf == '\x86': #134
+		F6(program)
+	if sbuf == '\x87': #135
+		F7(program)
+	if sbuf == '\x88': #136
+		F8(program)
+
---
+++ viui.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Based on: http://blog.skeltonnetworks.com/2010/03/python-curses-custom-menu/
+# by Matthew Bennett with changes by Andrew Scheller
+# A bunch of changes and optimisations for speakup done by Kieran Gee
+
+import time
+import curses
+import os
+import psutil
+import subprocess
+
+#Cursers init
+screen = curses.initscr() 
+curses.noecho()
+curses.cbreak() 
+curses.start_color()
+screen.keypad(1)
+curses.curs_set(0) #fix issues with espeakup
+
+# Highlighting
+curses.init_pair(1,curses.COLOR_WHITE, curses.COLOR_BLACK) 
+h = curses.color_pair(1) 
+n = curses.A_NORMAL 
+
+MENU = "menu"
+COMMAND = "command"
+EXITMENU = "exitmenu"
+
+menu_data = {
+'title': "VIUI Launcher", 'type': MENU, 'subtitle': "Please select an option...",
+'options':[
+	{ 'title': "Nano", 'type': COMMAND, 'command': 'nano' },
+		
+	{ 'title': "Mail", 'type': COMMAND, 'command': 'alpine' },
+
+	{ 'title': "eBook Reader (Currently just nano)", 'type': COMMAND, 'command': 'nano' },
+	
+	{ 'title': "Feed Reader", 'type': MENU, 'subtitle': "Select RSS feed to read",
+	'options': [
+
+		{ 'title': "ABC Perth", 'type': COMMAND, 'command': 'rm ~/rss.txt && echo "http://www.abc.net.au/local/rss/perth/all.xml" >> ~/rss.txt && newsbeuter -u ~/rss.txt' },
+
+		{ 'title': "ABC Politics", 'type': COMMAND, 'command': 'rm ~/rss.txt && echo "http://www.abc.net.au/news/feed/1534/rss.xml" >> ~/rss.txt && newsbeuter -u ~/rss.txt' },
+
+		{ 'title': "ABC Sport", 'type': COMMAND, 'command': 'rm ~/rss.txt && echo "http://abc.net.au/sport/syndicate/sport_all.xml" >> ~/rss.txt && newsbeuter -u ~/rss.txt' },
+
+		{ 'title': "Huffington Post", 'type': COMMAND, 'command': 'rm ~/rss.txt && echo "http://www.huffingtonpost.com/feeds/news.xml" >> ~/rss.txt && newsbeuter -u ~/rss.txt' },
+
+		{ 'title': "CNN", 'type': COMMAND, 'command': 'rm ~/rss.txt && echo "http://rss.cnn.com/rss/edition.rss" >> ~/rss.txt && newsbeuter -u ~/rss.txt' },
+		
+	]},
+	
+	{ 'title': "Web Browser", 'type': COMMAND, 'command': 'links' },
+	
+	{ 'title': "zsh", 'type': COMMAND, 'command': 'zsh' },
+
+	{ 'title': "Open Help", 'type': COMMAND, 'command': 'cmatrix' },
+	
+	{ 'title': "Shutdown Computer", 'type': MENU, 'subtitle': "Select shutdown option",
+	'options': [
+	
+		{'title': "Shutdown", 'type': COMMAND, 'command': 'sudo shutdown -h now' },
+	
+		{'title': "Restart", 'type': COMMAND, 'command': 'sudo shutdown -r now' },
+	]},
+]}
+
+
+
+
+# Main menu loop
+def runmenu(menu, parent):
+	if parent is None:
+		lastoption = "Exit"
+	else:
+		lastoption = "Return to %s menu" % parent['title']
+
+	optioncount = len(menu['options']) # find number of items in menu
+	pos=0 
+	oldpos=None
+	x = None 
+
+	#While enter key is not pressed
+	while x !=ord('\n'):
+		os.system("sudo pkill -f python\ inputdaemon"); #Move this somewhere nicer
+		if pos != oldpos:
+			oldpos = pos
+			screen.addstr(2,2, menu['title'], curses.A_STANDOUT) # Title for this menu
+			screen.addstr(4,2, menu['subtitle'], curses.A_BOLD) #Subtitle for this menu
+
+		# Display menu, pos is highlighted
+		for index in range(optioncount):
+			textstyle = n
+			if pos==index:
+				textstyle = h
+			screen.addstr(5+index,4, "%d - %s" % (index+1, menu['options'][index]['title']), textstyle)
+		# Add last option (exit)
+		textstyle = n
+		if pos==optioncount:
+			textstyle = h
+		screen.addstr(5+optioncount,4, "%d - %s" % (optioncount+1, lastoption), textstyle)
+		screen.refresh()
+
+		x = screen.getch() # Get user input
+
+		# Check user input
+		if x >= ord('1') and x <= ord(str(optioncount+1)):
+			pos = x - ord('0') - 1 # If the user presses a number
+		# elif x == 258: # Change pos based on down arrow
+		# 	if pos < optioncount:
+		# 		pos += 1
+		# 	else: 
+		# 		pos = 0
+		# elif x == 259: # Change pos based on down arrow
+		# 	if pos > 0:
+		# 		pos += -1
+		# 	else: 
+		# 		pos = optioncount
+	return pos # returnposition of curser
+
+# This function calls showmenu and then acts on the selected item
+def processmenu(menu, parent=None):
+	optioncount = len(menu['options'])
+	exitmenu = False
+	while not exitmenu: #Loop until the user exits the menu
+		getin = runmenu(menu, parent)
+		if getin == optioncount:
+			exitmenu = True
+		elif menu['options'][getin]['type'] == COMMAND:
+			curses.def_prog_mode()	# save curent curses environment
+			os.system('reset')
+			screen.clear() #clears previous screen
+			if 'newsbeuter' in menu['options'][getin]['command']:
+				subprocess.Popen(["nohup","sudo","python","viinputdaemon.py","newsbeuter","&"])
+			else: 
+				subprocess.Popen(["nohup","sudo","python","viinputdaemon.py",menu['options'][getin]['command'],"&"])
+			os.system(menu['options'][getin]['command']) # run the command
+			screen.clear() #clears previous screen on key press and updates display based on pos
+			curses.reset_prog_mode() # reset to 'current' curses environment
+			curses.curs_set(1)		 # reset doesn't do this right
+			curses.curs_set(0)
+		elif menu['options'][getin]['type'] == MENU:
+			screen.clear() #clears previous screen on key press and updates display based on pos
+			processmenu(menu['options'][getin], menu) # display the submenu
+			screen.clear() #clears previous screen on key press and updates display based on pos
+		elif menu['options'][getin]['type'] == EXITMENU:
+			exitmenu = True
+
+# Main program
+processmenu(menu_data)
+curses.endwin() # Exits the cursers menu
+os.system('touch nohup.out && rm nohup.out') # Remove the nohup output, fix for sisuation where no program was run
+os.system('clear')
