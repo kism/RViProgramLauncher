653eea60cd3398688502b7b095442a8b8729fc22
Kieran Gee <kieran.lost.the.game@gmail.com>
2015-10-27 19:50:13 +0800

    Cleanup

--- viui.py
+++ viui.py
@@ -3,6 +3,7 @@
 # Based on: http://blog.skeltonnetworks.com/2010/03/python-curses-custom-menu/
 # by Matthew Bennett with changes by Andrew Scheller
 # A bunch of changes and optimisations for speakup done by Kieran Gee
+# No idea what public liscense ¯\_(ツ)_/¯
 
 import time
 import curses
@@ -10,18 +11,18 @@ import os
 import psutil
 import subprocess
 
-#Cursers init
-screen = curses.initscr() 
+# Curses init
+screen = curses.initscr()
 curses.noecho()
-curses.cbreak() 
+curses.cbreak()
 curses.start_color()
 screen.keypad(1)
 curses.curs_set(0) # try to fix issues with espeakup
 
 # Highlighting
 curses.init_pair(1,curses.COLOR_WHITE, curses.COLOR_BLACK) # fix issues with espeakup
-h = curses.color_pair(1) 
-n = curses.A_NORMAL 
+h = curses.color_pair(1)
+n = curses.A_NORMAL
 
 MENU = "menu"
 COMMAND = "command"
@@ -31,37 +32,22 @@ menu_data = {
 'title': "VIUI Launcher", 'type': MENU, 'subtitle': "Please select an option...",
 'options':[
 	{ 'title': "Nano", 'type': COMMAND, 'command': 'nano' },
-		
 	{ 'title': "Mail", 'type': COMMAND, 'command': 'alpine' },
-
 	{ 'title': "eBook Reader (Currently just nano)", 'type': COMMAND, 'command': 'nano' },
-	
 	{ 'title': "Feed Reader", 'type': MENU, 'subtitle': "Select RSS feed to read",
 	'options': [
-
 		{ 'title': "ABC Perth", 'type': COMMAND, 'command': 'rm ~/rss.txt && echo "http://www.abc.net.au/local/rss/perth/all.xml" >> ~/rss.txt && newsbeuter -u ~/rss.txt' },
-
 		{ 'title': "ABC Politics", 'type': COMMAND, 'command': 'rm ~/rss.txt && echo "http://www.abc.net.au/news/feed/1534/rss.xml" >> ~/rss.txt && newsbeuter -u ~/rss.txt' },
-
 		{ 'title': "ABC Sport", 'type': COMMAND, 'command': 'rm ~/rss.txt && echo "http://abc.net.au/sport/syndicate/sport_all.xml" >> ~/rss.txt && newsbeuter -u ~/rss.txt' },
-
 		{ 'title': "Huffington Post", 'type': COMMAND, 'command': 'rm ~/rss.txt && echo "http://www.huffingtonpost.com/feeds/news.xml" >> ~/rss.txt && newsbeuter -u ~/rss.txt' },
-
 		{ 'title': "CNN", 'type': COMMAND, 'command': 'rm ~/rss.txt && echo "http://rss.cnn.com/rss/edition.rss" >> ~/rss.txt && newsbeuter -u ~/rss.txt' },
-		
 	]},
-	
 	{ 'title': "Web Browser", 'type': COMMAND, 'command': 'links' },
-	
-	 { 'title': "zsh", 'type': COMMAND, 'command': 'zsh' }, # may have issues
-
+	{ 'title': "zsh", 'type': COMMAND, 'command': 'zsh' }, # may have issues
 	{ 'title': "Open Help", 'type': COMMAND, 'command': 'cmatrix' },
-	
 	{ 'title': "Shutdown Computer", 'type': MENU, 'subtitle': "Select shutdown option",
 	'options': [
-	
 		{'title': "Shutdown", 'type': COMMAND, 'command': 'sudo shutdown -h now' },
-	
 		{'title': "Restart", 'type': COMMAND, 'command': 'sudo shutdown -r now' },
 	]},
 ]}
@@ -74,9 +60,9 @@ def runmenu(menu, parent):
 		lastoption = "Return to %s menu" % parent['title']
 
 	optioncount = len(menu['options']) # find number of items in menu
-	pos=0 
+	pos=0
 	oldpos=None
-	x = None 
+	x = None
 
 	# While enter key is not pressed
 	while x !=ord('\n'):
@@ -92,6 +78,7 @@ def runmenu(menu, parent):
 			if pos==index:
 				textstyle = h
 			screen.addstr(5+index,4, "%d - %s" % (index+1, menu['options'][index]['title']), textstyle)
+		
 		# Add last option (exit)
 		textstyle = n
 		if pos==optioncount:
@@ -104,6 +91,7 @@ def runmenu(menu, parent):
 		# Check user input
 		if x >= ord('1') and x <= ord(str(optioncount+1)):
 			pos = x - ord('0') - 1 # If the user presses a number
+	
 	return pos # returnposition of curser
 
 # This function calls showmenu and then acts on the selected item
@@ -119,25 +107,25 @@ def processmenu(menu, parent=None):
 			curses.def_prog_mode() # Save menu status
 			os.system('reset')
 			screen.clear()
-			
+	
 			# Run input daemon in background
 			if 'newsbeuter' in menu['options'][getin]['command']:
-				subprocess.Popen(["nohup","sudo","python","viinputdaemon.py","newsbeuter","&"]) 
-			else: 
-				subprocess.Popen(["nohup","sudo","python","viinputdaemon.py",menu['options'][getin]['command'],"&"]) 
-			
+				subprocess.Popen(["nohup","sudo","python","viinputdaemon.py","newsbeuter","&"])
+			else:
+				subprocess.Popen(["nohup","sudo","python","viinputdaemon.py",menu['options'][getin]['command'],"&"])
+	
 			# Run Program
 			os.system(menu['options'][getin]['command'])
-			
+	
 			# Cleanup
-			screen.clear() 
-			curses.reset_prog_mode() 
-			curses.curs_set(1)		 
+			screen.clear()
+			curses.reset_prog_mode()
+			curses.curs_set(1)
 			curses.curs_set(0)
 		elif menu['options'][getin]['type'] == MENU:
-			screen.clear() 
+			screen.clear()
 			processmenu(menu['options'][getin], menu) # display the submenu
-			screen.clear() 
+			screen.clear()
 		elif menu['options'][getin]['type'] == EXITMENU:
 			exitmenu = True
 
